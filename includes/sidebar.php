20162ff5c1b4644f3919e0ea1a2ff7f9b2948f38
Samuel-Fortune <samuelchibu04@gmail.com>
2022-12-17 08:46:59 +0100

    updated

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,33 +1,9 @@
 <div class="col-md-4">
 
-<?php
-if (isset($_POST['submit'])) {
-
-    $search = $_POST['search'];
-    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
-    $search_query = mysqli_query($connection, $query);
-
-    if (!$search_query) {
-        die("QUERY FAILED" . mysqli_error($connection));
-
-    }
-
-    $count = mysqli_num_rows($search_query);
-    if ($count == 0) {
-
-        echo "<h1>NO RESULT</h1>";
-
-    } else {
-        echo "<h1>SOME RESULT</h1>";
-    }
-
-}
-
-?>
 <!-- Blog Search Well -->
 <div class="well">
     <h4>Blog Search</h4>
-    <form action="" method="post">
+    <form action="search.php" method="post">
         <div class="input-group">
             <input name="search" type="text" class="form-control">
             <span class="input-group-btn">
