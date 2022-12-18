7aa11ab734bcc186d423e0f582f6e84953c3d8be
Samuel-Fortune <samuelchibu04@gmail.com>
2022-12-18 20:41:34 +0100

    updated

--- admin/posts.php
+++ admin/posts.php
@@ -22,72 +22,37 @@ include "includes/admin_navigation.php";
                             <small>Author</small>
                         </h1>
 
-                        <table class="table table-bordered table-hover">
-                            <thead>
+                        <?php 
+                        
+                        if(isset($_GET['source'])){
+                          $source = $_GET['source'];
+                          
+                          
 
+                        }else{
+                            $source ='';
+                        }
+                        switch($source) {
+                            case 'add_post';
+                            include "includes/add_post.php";
+                            break;
 
-                                <tr>
+                            case '100';
+                            echo "NICE 100";
+                            break;
 
+                            case '200';
+                            echo "NICE 200";
+                            break;
 
-                                    <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Title</th>
-                                    <th>Category</th>
-                                    <th>Status</th>
-                                    <th>Image</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Dates</th>
-                                </tr>
+                            default:
 
-                            </thead>
-                            <tbody>
+                            include "includes/view_all_posts.php";
 
+                            break;
 
-                            <?php
-
-$query = "SELECT * FROM posts";
-$select_posts = mysqli_query($connection, $query);
-
-while ($row = mysqli_fetch_assoc($select_posts)) {
-    $post_id = $row['post_id'];
-    $post_author = $row['post_author'];
-    $post_title = $row['post_title'];
-    $post_category_id = $row['post_category_id'];
-    $post_status = $row['post_status'];
-    $post_image = $row['post_image'];
-    $post_tags = $row['post_tags'];
-    $post_comment_count = $row['post_comment_count'];
-    $post_date = $row['post_date'];
-
-    echo "<tr>";
-    echo "<td>$post_id</td>";
-    echo "<td>$post_author</td>";
-    echo "<td>$post_title</td>";
-    echo "<td>$post_category_id</td>";
-    echo "<td>$post_status</td>";
-    echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
-    echo "<td>$post_tags</td>";
-    echo "<td>$post_comment_count</td>";
-    echo "<td>$post_date</td>";
-    echo "</tr>";
-
-}
-?>
-
-                                <td>10</td>
-                                <td>Fortune</td>
-                                <td>Bootstrap framework</td>
-                                <td>Bootstrap</td>
-                                <td>Status</td>
-                                <td>Images</td>
-                                <td>Tags</td>
-                                <td>Comments</td>
-                                <td>Date</td>
-
-                        </tbody>
-
-                        </table>
+                        }
+                        ?>
 
 
 
